e0f28a9aab3fc97b0abc458ea74c75e2a60351e0
BryanBac <bebac@correo.url.edu.gt>
2023-04-09 13:43:18 -0600

    git api inicio

--- portafolio/src/components/git-container.js
+++ portafolio/src/components/git-container.js
@@ -1,7 +1,30 @@
-export default function GitContainer(){
-    return (
-        <div className="TwoGrid">
-            
-        </div>
-    )
+import { useEffect, useState } from "react";
+
+export default function GitContainer() {
+  const [search, setSearch] = useState("");
+  const [user, setUser] = useState({});
+  const fetchUser = async () => {
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${search}/repos`
+      );
+      const data = await response.json();
+      setUser(data);
+      console.log(user[0]);
+    } catch (error) {
+      console.log("error", error.message);
+    }
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for User"
+        onChange={(e)=>setSearch(e.target.value)}
+        className="input_search"
+      />
+      <button onClick={fetchUser}>Buscar</button>
+      <div className="TwoGrid"></div>
+    </div>
+  );
 }
