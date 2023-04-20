a5199e4b9e4fa4c1eb4af7511c8b024310fc7cc4
BryanBac <bebac@correo.url.edu.gt>
2023-04-20 07:32:08 -0600

    avances en git api

--- portafolio/src/components/git-container.js
+++ portafolio/src/components/git-container.js
@@ -1,31 +1,36 @@
 import { useEffect, useState } from "react";
 import { Octokit } from "octokit";
 
-const octokit = new Octokit({ 
+const octokit = new Octokit({
   auth: process.env.TOKEN,
 });
 
-
-
 export default function GitContainer() {
-  const [user, setUser] = useState({});
-  const fetchUser = async () => {
-    try {
+  const [user, setUser] = useState([]);
+  const [datosUser, setDatosUser] = useState([]);
+  useEffect(() => {
+    async function obtener() {
       const response = await fetch(
         `https://api.github.com/users/BryanBac/repos`
       );
       const data = await response.json();
       setUser(data);
-      console.log(user);
-      return data;
-    } catch (error) {
-      console.log("error", error.message);
     }
-  };
+    obtener();
+  }, []);
+  useEffect(()=>{
+    var listado = []
+    for (var i = 0; i< 30; i++){
+      listado.push(<div>{user[i]}</div>)
+      console.log(user.at(i))
+    }
+    setDatosUser(listado)
+  },[user])
   return (
     <div>
-      <button onClick={fetchUser}>Buscar</button>
-      <div className="TwoGrid"></div>
+      <button onClick={()=>console.log(datosUser)}>Buscar</button>
+      <div className="aboutContainer">
+      </div>
     </div>
   );
 }
